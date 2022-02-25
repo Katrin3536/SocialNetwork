4f8cc12ed71a192732289b763bbe7be630956279
Dudko Kate <ekaterina_dr@tut.by>
2022-02-25 15:39:52 +0300

    added 9 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Technologies from "./components/Technologies";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+          <Header/>
+          <Technologies/>
+      </div>
   );
 }
 
