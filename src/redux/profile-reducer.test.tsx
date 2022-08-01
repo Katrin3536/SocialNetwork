a34da93fa4caabd8a0a96644ba0ec8cff555762e
Katrin3536 <Ekaterina_dr@tut.by>
2022-08-02 00:45:15 +0300

    added unit-tests for profileReducer

--- src/redux/profile-reducer.test.tsx
+++ src/redux/profile-reducer.test.tsx
@@ -2,7 +2,10 @@ import profileReducer, {
     addPostActionCreator,
     deletePostActionCreator,
     ProfileActionType,
-    ProfilePageType
+    ProfilePageType,
+    ProfileType,
+    setStatusAC,
+    setUserProfileAC
 } from './profile-reducer';
 
 let state: ProfilePageType;
@@ -13,7 +16,27 @@ beforeEach(() => {
             {id: 1, message: 'Hi! How are you?', likeCount: 10},
             {id: 2, message: 'My first post', likeCount: 15},
         ],
-        profile: null,
+        profile: {
+            aboutMe: 'Katerina',
+            userId: 100,
+            lookingForAJob: true,
+            lookingForAJobDescription: 'string',
+            fullName: 'string',
+            contacts: {
+                github: 'string',
+                vk: 'string',
+                facebook: 'string',
+                instagram: 'string',
+                twitter: 'string',
+                website: 'string',
+                youtube: 'string',
+                mainLink: 'string',
+            },
+            photos: {
+                small: 'small',
+                large: 'large',
+            }
+        },
         status: ''
     };
 });
@@ -40,4 +63,38 @@ test('after deleting length of messages should not be decremented if id incorrec
     let action: ProfileActionType = deletePostActionCreator(10);
     let newState = profileReducer(state, action);
     expect(newState.postData.length).toBe(2);
+});
+
+test('Profile is setted success', () => {
+
+    let newProfile: ProfileType = {
+        aboutMe: 'Hello',
+        userId: 23,
+        lookingForAJob: true,
+        lookingForAJobDescription: 'string',
+        fullName: 'string',
+        contacts: {
+            github: 'string',
+            vk: 'string',
+            facebook: 'string',
+            instagram: 'string',
+            twitter: 'string',
+            website: 'string',
+            youtube: 'string',
+            mainLink: 'string',
+        },
+        photos: {
+            small: 'small',
+            large: 'large',
+        }
+    };
+    let action: ProfileActionType = setUserProfileAC(newProfile);
+    let newState = profileReducer(state, action);
+    expect(newState.profile).toBe(newProfile);
+});
+
+test('Status is changed success', () => {
+    let action: ProfileActionType = setStatusAC('I am ready to work');
+    let newState = profileReducer(state, action);
+    expect(newState.status).toBe('I am ready to work');
 });
