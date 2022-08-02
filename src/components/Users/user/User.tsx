d8b500a0217c25e88a043c5d2f0e3e59e6899b49
Katrin3536 <Ekaterina_dr@tut.by>
2022-08-03 01:32:43 +0300

    refactoring part of code

---
+++ src/components/Users/user/User.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import s from './User.module.css';
+import photo from '../../assets/images/avatarIcon.jpg';
+import {UsersType} from '../../redux/users-reducer';
+import {NavLink} from 'react-router-dom';
+
+export type UserPropsType = {
+    follow: (id: number) => void,
+    unfollow: (id: number) => void,
+    followingInProgress: Array<number>,
+    user: UsersType
+}
+
+export const User: React.FC<UserPropsType> = ({user, follow, followingInProgress,unfollow  }) => {
+    return (
+        <div>
+            <span>
+                <div>
+                    <NavLink to={'/profile/' + user.id}><img className={s.usersPhoto}
+                                                          src={user.photos.small != null ? user.photos.small : photo}/>
+                    </NavLink>
+                </div>
+                <div> {user.followed
+                    ? <button disabled={followingInProgress.some(id => id === user.id)} onClick={
+                        () => {
+                           unfollow(user.id);
+                        }
+                    }>Unfollow</button>
+                    : <button disabled={followingInProgress.some(id => id === user.id)} onClick={
+                        () => {
+                            follow(user.id);
+                        }
+                    }>Follow</button>
+                }
+                </div>
+            </span>
+            <span>
+                <span>
+                    <div>{user.name}</div>
+                    <div>{user.status}</div>
+                </span>
+                <span>
+                    <div>{'u.location.country'}</div>
+                    <div>{'u.location.city'}</div>
+                </span>
+            </span>
+        </div>
+
+    );
+};
---
+++ src/components/common/pagination/Pagination.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import s from './Users.module.css';
+
+export type PaginationPropsType = {
+    onPageChanged: (p: number) => void,
+    totalUsersCount: number,
+    pageSize: number,
+    currentPage: number,
+}
+
+export const Pagination:React.FC<PaginationPropsType> = ({totalUsersCount,pageSize, currentPage,onPageChanged }) => {
+    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    return (
+        <div>
+            {pages.map((p, index) => {
+                return <span
+                    className={currentPage === p && s.selected}
+                    key={index}
+                    onClick={(e) => onPageChanged(p)
+                    }>{p}</span>;
+            })}
+        </div>
+    );
+};
